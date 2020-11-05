f5d71bc0d08e9cda2c4f45b26b92db3640fcb8a2
Ryan Albertson <ryan061999@gmail.com>
2020-11-05 14:26:19 -0500

    worked on login page

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,7 +2,6 @@ const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
 const container = document.getElementById("container");
 
-
 signUpButton.addEventListener("click", () =>
     container.classList.add('right-panel-active')
 );
