97d4db8277a3d7415717ff31628340126d61b92a
Ryan Albertson <ryan061999@gmail.com>
2020-11-08 17:19:51 -0500

    Update home.php

--- home/home.php
+++ home/home.php
@@ -7,3 +7,19 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 ?>
+
+<!-- DOCTYPE html -->
+<html>
+  <head>
+    <title>Welcome to BeerCo</title>
+    <link rel="stylesheet" href="../resources/css/404.css" media="Screen" type="text/css"/>
+  </head>
+  <body>
+    <div>
+      <h1>Welcome to the BeerCo Beer Kiosk</h1>
+      <form>
+        <input type="button" value="Return to previous page" onclick="history.back()">
+      </form>
+    </div>
+  </body>
+</html>
