14b4efccfc7a0316fcaa29621ea0b18924a61ff4
alhusseindev <55993187+alhusseindev@users.noreply.github.com>
2020-12-08 19:59:04 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+
+const openNav = document.querySelector('.hamburger');
+const closeNav = document.querySelector('.nav__close');
+const nav = document.querySelector('.nav__list');
+
+openNav.addEventListener('click', () => {
+    const navLeft = nav.getBoundingClientRect().left
+    if(navLeft < 0){
+        nav.style.left = 0;
+        document.body.classList.add('active');
+    }
+});
+
+
+
+closeNav.addEventListener('click', () => {
+   const navLeft = nav.getBoundingClientRect().left
+    if(navLeft === 0){
+        nav.style.left = '-40rem';
+        document.body.classList.remove('active');
+    }
+});
+
+
+//smooth scroll
+// ... rest syntax
+const scrollLink = [... document.querySelectorAll('.scroll-link')];
+scrollLink.forEach(link => {
+    link.addEventListener('click', e => {
+        const id = e.target.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+        const position = element.offsetTop;
+
+        window.scrollTo({
+            left: 0,
+            top: position
+        })
+
+        nav.style.left = "-40rem";
+        document.body.classList.remove("active");
+    })
+})
